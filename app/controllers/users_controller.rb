61afee840c64000d5c1167dfe40042e3c38f1430
yohhei someya <youhei19880130@gmail.com>
2014-09-17 23:18:10 +0900

    終了前(but 登録ができない。。。)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(params[:users])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 end

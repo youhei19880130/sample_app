b5763e2673d69fd585988bff878a1f86f3e30d5b
yohhei someya <youhei19880130@gmail.com>
2014-09-18 16:28:56 +0900

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,13 +4,6 @@ describe "User pages" do
 
   subject { page }
 
-  describe "signup page" do
-    before {visit signup_path}
-
-    it {should have_selector('h1', text: 'Sign up')}
-    it {should have_selector('title', text: 'Sign up')}
-  end
-
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }

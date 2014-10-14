38f82bc5440451e451e7931f34d5cf288e2fb194
yohhei someya <youhei19880130@gmail.com>
2014-10-14 21:49:52 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.routes.draw do
   resources :users
-  root 'static_pages#home'
+  root to: 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
